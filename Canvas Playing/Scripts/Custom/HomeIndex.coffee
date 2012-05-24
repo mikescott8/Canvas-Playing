5be2c34abc0462301ad9c3e583b2ccc5a2d5d05f
Mike Hamilton <mike@mikescott8.net>
2012-05-24 07:40:22 -0700

    First real checkin
    Checking in for first real time

--- Canvas Playing/Scripts/Custom/HomeIndex.coffee
+++ Canvas Playing/Scripts/Custom/HomeIndex.coffee
@@ -1,52 +1,136 @@
 ﻿class drawnItem
-	constructor: () ->
-		@x = 5
-		@y = 5
+	x = 0
+	y = 0
+	xDir = 1
+	yDir = 1
+
+	constructor: (@x, @y) ->
 
 	drawItem: (ctxt) ->
-		console.log 'base drawItem at ' + @x.toString() + ',' + @y.toString()
+		#console.log 'base drawItem at ' + @x.toString() + ',' + @y.toString()
 		ctxt
 
 	moveBy: (x,y) ->
-		#console.log 'base moveBy ' + @x + ',' + @y
-		@x = @x + x
-		@y = @y + y
+		#console.log 'base moveBy ' + x + ',' + y
+		@x = @x + (x * @xDir)
+		@y = @y + (y * @yDir)
 		@checkBounds()
 		@
 
 	checkBounds: ->
 		#console.log 'checkBounds ' + @x + ',' + @y
-		unless @x < 500 then @x = 0
-		unless @y < 500 then @y = 0
+		if !(0 < @x  < 500)
+			@xDir * -1
+			if @x < 0
+				@x = 0
+				@changeXDir()
+			if @x > 500
+				@x = 500
+				@changeXDir()
+		if !(0 < @y  < 500)
+			@yDir * -1
+			if @y < 0
+				@y = 0
+				@changeYDir()
+			if @y > 500
+				@y = 500
+				@changeYDir()
 		#console.log 'checkBounds ' + @x + ',' + @y
 		@
 
-class circleItem extends drawnItem
-	constructor: () ->
+	changeXDir: () ->
+		@xDir = @xDir * -1
+		@xDir
+
+	changeYDir: () ->
+		@yDir = @yDir * -1
+		@yDir
+
+class squareItem extends drawnItem
+	width = 3
+	height = 3
+	fillStyle = 'rgb(500,0,0)'
+
+	constructor: (@width, @height) ->
 		super
 
 	drawItem: (ctxt) ->
-		console.log 'circleItem.drawItem'
-
-		ctxt.fillStyle = "rgb(500,0,0)"
-		ctxt.fillRect @x, @y, 5, 5
+		#console.log 'drawItem'
+		ctxt.fillStyle = @fillStyle
+		ctxt.fillRect @x, @y, @width, @height
 		super ctxt
 
+
 class game
-	@ball
-
 	constructor: (@ctxt) ->
-		@ball = new circleItem()
-		@ball.moveBy 20,20
-		@ball.drawItem @ctxt
+		@moveMax=20
+		@moveTime = 100
 
-	moveBall: () =>
-		console.log 'game moveBall'
-		@ball.moveBy 13, 9
-		@ball.drawItem @ctxt
-		setTimeout @moveBall, 1000
+		@items = []
+		@addBall()
+		@addBall()
+		@addBall()
+
+	updateBoard: () =>
+		#console.log 'updateBoard'
+		@moveItem item for item in @items
+
+		if @active 
+			setTimeout @updateBoard, @moveTime
 		@
+
+	moveItem: (item) =>
+		#console.log 'game moveItem'
+		#console.log @moveMax
+
+		xMove = Math.floor (Math.random() * @moveMax)
+		yMove = Math.floor (Math.random() * @moveMax)
+
+		item.moveBy xMove, yMove
+		item.drawItem @ctxt
 	
+	toggleActive: =>
+		@active = !@active
+		if @active 
+			@updateBoard()
+		@
+
+	clearScreen: =>
+		@ctxt.clearRect 0, 0, 500, 500
+		@
+
+	addBall: =>
+		width = (Math.floor (Math.random() * 12)) + 5
+		height = (Math.floor (Math.random() * 12)) + 5
+		
+		item = new squareItem width, height
+
+		r = Math.floor (Math.random() * 255)
+		g = Math.floor (Math.random() * 255)
+		b = Math.floor (Math.random() * 255)
+
+		item.fillStyle = "rgb(#{r}, #{g}, #{b})"
+
+		x = Math.floor (Math.random() * 500)
+		y = Math.floor (Math.random() * 500)
+
+		item.x = x
+		item.y = y
+		item.xDir = if (x % 2 == 0) then 1 else -1
+		#console.log item.xDir
+		item.yDir = if (y % 2 == 0) then 1 else -1
+		#console.log item.yDir
+
+		#console.log item
+		@items.push item
+		@items
+
+	removeBall: =>
+		@items.pop()
+
+	removeAll: =>
+		@items.pop() while @items.length != 0
+
 @game = window.game ? {}
 
 jQuery ->
@@ -54,5 +138,9 @@ jQuery ->
 	context = canvas.getContext('2d')
 
 	@game = new game(context)
-	@game.moveBall()
-	#setInterval "window.game.moveBall()", 1000
+
+	$('#btnActive').on 'click', @game.toggleActive
+	$('#btnClear').on 'click', @game.clearScreen
+	$('#btnAdd').on 'click', @game.addBall
+	$('#btnRemove').on 'click', @game.removeBall
+	$('#btnRemoveAll').on 'click', @game.removeAll
--- Canvas Playing/Scripts/Custom/HomeIndex.js
+++ Canvas Playing/Scripts/Custom/HomeIndex.js
@@ -1,75 +1,183 @@
 (function() {
-  var circleItem, drawnItem, game, _ref,
+  var drawnItem, game, squareItem, _ref,
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   drawnItem = (function() {
+    var x, xDir, y, yDir;
 
-    function drawnItem() {
-      this.x = 5;
-      this.y = 5;
+    x = 0;
+
+    y = 0;
+
+    xDir = 1;
+
+    yDir = 1;
+
+    function drawnItem(x, y) {
+      this.x = x;
+      this.y = y;
     }
 
     drawnItem.prototype.drawItem = function(ctxt) {
-      console.log('base drawItem at ' + this.x.toString() + ',' + this.y.toString());
       return ctxt;
     };
 
     drawnItem.prototype.moveBy = function(x, y) {
-      this.x = this.x + x;
-      this.y = this.y + y;
+      this.x = this.x + (x * this.xDir);
+      this.y = this.y + (y * this.yDir);
       this.checkBounds();
       return this;
     };
 
     drawnItem.prototype.checkBounds = function() {
-      if (!(this.x < 500)) this.x = 0;
-      if (!(this.y < 500)) this.y = 0;
+      var _ref, _ref2;
+      if (!((0 < (_ref = this.x) && _ref < 500))) {
+        this.xDir * -1;
+        if (this.x < 0) {
+          this.x = 0;
+          this.changeXDir();
+        }
+        if (this.x > 500) {
+          this.x = 500;
+          this.changeXDir();
+        }
+      }
+      if (!((0 < (_ref2 = this.y) && _ref2 < 500))) {
+        this.yDir * -1;
+        if (this.y < 0) {
+          this.y = 0;
+          this.changeYDir();
+        }
+        if (this.y > 500) {
+          this.y = 500;
+          this.changeYDir();
+        }
+      }
       return this;
+    };
+
+    drawnItem.prototype.changeXDir = function() {
+      this.xDir = this.xDir * -1;
+      return this.xDir;
+    };
+
+    drawnItem.prototype.changeYDir = function() {
+      this.yDir = this.yDir * -1;
+      return this.yDir;
     };
 
     return drawnItem;
 
   })();
 
-  circleItem = (function(_super) {
+  squareItem = (function(_super) {
+    var fillStyle, height, width;
 
-    __extends(circleItem, _super);
+    __extends(squareItem, _super);
 
-    function circleItem() {
-      circleItem.__super__.constructor.apply(this, arguments);
+    width = 3;
+
+    height = 3;
+
+    fillStyle = 'rgb(500,0,0)';
+
+    function squareItem(width, height) {
+      this.width = width;
+      this.height = height;
+      squareItem.__super__.constructor.apply(this, arguments);
     }
 
-    circleItem.prototype.drawItem = function(ctxt) {
-      console.log('circleItem.drawItem');
-      ctxt.fillStyle = "rgb(500,0,0)";
-      ctxt.fillRect(this.x, this.y, 5, 5);
-      return circleItem.__super__.drawItem.call(this, ctxt);
+    squareItem.prototype.drawItem = function(ctxt) {
+      ctxt.fillStyle = this.fillStyle;
+      ctxt.fillRect(this.x, this.y, this.width, this.height);
+      return squareItem.__super__.drawItem.call(this, ctxt);
     };
 
-    return circleItem;
+    return squareItem;
 
   })(drawnItem);
 
   game = (function() {
 
-    game.ball;
-
     function game(ctxt) {
       this.ctxt = ctxt;
-      this.moveBall = __bind(this.moveBall, this);
-      this.ball = new circleItem();
-      this.ball.moveBy(20, 20);
-      this.ball.drawItem(this.ctxt);
+      this.removeAll = __bind(this.removeAll, this);
+      this.removeBall = __bind(this.removeBall, this);
+      this.addBall = __bind(this.addBall, this);
+      this.clearScreen = __bind(this.clearScreen, this);
+      this.toggleActive = __bind(this.toggleActive, this);
+      this.moveItem = __bind(this.moveItem, this);
+      this.updateBoard = __bind(this.updateBoard, this);
+      this.moveMax = 20;
+      this.moveTime = 100;
+      this.items = [];
+      this.addBall();
+      this.addBall();
+      this.addBall();
     }
 
-    game.prototype.moveBall = function() {
-      console.log('game moveBall');
-      this.ball.moveBy(13, 9);
-      this.ball.drawItem(this.ctxt);
-      setTimeout(this.moveBall, 1000);
+    game.prototype.updateBoard = function() {
+      var item, _i, _len, _ref;
+      _ref = this.items;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        item = _ref[_i];
+        this.moveItem(item);
+      }
+      if (this.active) setTimeout(this.updateBoard, this.moveTime);
       return this;
+    };
+
+    game.prototype.moveItem = function(item) {
+      var xMove, yMove;
+      xMove = Math.floor(Math.random() * this.moveMax);
+      yMove = Math.floor(Math.random() * this.moveMax);
+      item.moveBy(xMove, yMove);
+      return item.drawItem(this.ctxt);
+    };
+
+    game.prototype.toggleActive = function() {
+      this.active = !this.active;
+      if (this.active) this.updateBoard();
+      return this;
+    };
+
+    game.prototype.clearScreen = function() {
+      this.ctxt.clearRect(0, 0, 500, 500);
+      return this;
+    };
+
+    game.prototype.addBall = function() {
+      var b, g, height, item, r, width, x, y;
+      width = (Math.floor(Math.random() * 12)) + 5;
+      height = (Math.floor(Math.random() * 12)) + 5;
+      item = new squareItem(width, height);
+      r = Math.floor(Math.random() * 255);
+      g = Math.floor(Math.random() * 255);
+      b = Math.floor(Math.random() * 255);
+      item.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+      x = Math.floor(Math.random() * 500);
+      y = Math.floor(Math.random() * 500);
+      item.x = x;
+      item.y = y;
+      item.xDir = x % 2 === 0 ? 1 : -1;
+      item.yDir = y % 2 === 0 ? 1 : -1;
+      this.items.push(item);
+      return this.items;
+    };
+
+    game.prototype.removeBall = function() {
+      return this.items.pop();
+    };
+
+    game.prototype.removeAll = function() {
+      var _results;
+      _results = [];
+      while (this.items.length !== 0) {
+        _results.push(this.items.pop());
+      }
+      return _results;
     };
 
     return game;
@@ -83,7 +191,11 @@
     canvas = $("#myCanvas").get(0);
     context = canvas.getContext('2d');
     this.game = new game(context);
-    return this.game.moveBall();
+    $('#btnActive').on('click', this.game.toggleActive);
+    $('#btnClear').on('click', this.game.clearScreen);
+    $('#btnAdd').on('click', this.game.addBall);
+    $('#btnRemove').on('click', this.game.removeBall);
+    return $('#btnRemoveAll').on('click', this.game.removeAll);
   });
 
 }).call(this);
